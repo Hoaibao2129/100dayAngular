21d2fa0c72ba8fcbfb5096d5d5484a33ead87360
HoaiBaoNguyen <hoaibao2129@gmail.com>
2023-04-22 08:47:50 +0700

    add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeCoponentsModule } from './components/homeComponents/home-coponents.module';
+import { IndexComponent } from './components/homeComponents/index/index.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+  },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeCoponentsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
